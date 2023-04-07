415a19305cf1b4455b903617a348fad0afe798d3
stobertonf <stobertonf@gmail.com>
2023-04-07 17:55:27 -0300

    feat(Login): Adding new color.

--- lib/app/shared/styles/color_palette.dart
+++ lib/app/shared/styles/color_palette.dart
@@ -4,4 +4,4 @@ const Color loginButton = Color(0xffc77493);
 const Color subtitlesFont = Color(0xff767676);
 const Color forgotPasswordText = Color(0xff007CD7);
 const Color homeBackgroundColor = Color(0xffd1e9ff);
-const Color textFormFieldFontColor = Color(0xa8a8a8ff);
+const Color textFormFieldFontColor = Color(0xffa8a8a8);
