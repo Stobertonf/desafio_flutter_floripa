8bc464ab230cd3e4ce30303a574df7e92c16d60d
stobertonf <stobertonf@gmail.com>
2023-04-07 16:41:26 -0300

    feat(Login): Adding new colors.

--- lib/app/shared/styles/color_palette.dart
+++ lib/app/shared/styles/color_palette.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const Color loginButton = Color(0xffc77493);
 const Color subtitlesFont = Color(0xff767676);
 const Color forgotPasswordText = Color(0xff007CD7);
+const Color homeBackgroundColor = Color(0xffd1e9ff);
+const Color textFormFieldFontColor = Color(0xa8a8a8ff);
