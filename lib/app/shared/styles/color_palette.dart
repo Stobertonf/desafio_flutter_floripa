317222a4a5d47df5acacc2b55260eb0a206278f6
stobertonf <stobertonf@gmail.com>
2023-04-07 15:25:43 -0300

    feat(Login): Creating the color palette file.

---
+++ lib/app/shared/styles/color_palette.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color loginButton = Color(0xffc77493);
+const Color subtitlesFont = Color(0xff767676);
+const Color forgotPasswordText = Color(0xff007CD7);
